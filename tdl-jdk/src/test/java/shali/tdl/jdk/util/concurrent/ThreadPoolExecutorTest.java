9db2781da26654396f61766cc1da75bf07ae7f75
shali <shali@gmail.com>
2023-08-20 22:46:18 +0800

    feat: jdk 内置线程池

--- tdl-jdk/src/test/java/shali/tdl/jdk/util/concurrent/ThreadPoolExecutorTest.java
+++ tdl-jdk/src/test/java/shali/tdl/jdk/util/concurrent/ThreadPoolExecutorTest.java
@@ -1,28 +1,25 @@
 package shali.tdl.jdk.util.concurrent;
 
-import cn.hutool.core.thread.ExecutorBuilder;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-public class ThreadPoolExecutorTest {
+class ThreadPoolExecutorTest {
+    /**
+     * Java 内置的线程池
+     */
     @Test
-    public void addOneThread() {
-        ThreadPoolExecutor threadPoolExecutor = ExecutorBuilder.create()
-                // 初始线程数
-                .setCorePoolSize(2)
-                // 最大线程数
-                .setMaxPoolSize(5)
-                // 线程超时时间，单位秒
-                .setKeepAliveTime(60)
-                .build();
+    void addOneThread() {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 3, 60, TimeUnit.NANOSECONDS, new LinkedBlockingQueue<>(10));
+        Assertions.assertTrue(threadPoolExecutor instanceof ThreadPoolExecutor);
         Thread testThread = new Thread(() -> {
-            System.out.println(Thread.currentThread().getName());
-            System.out.println(LocalDateTime.now().getSecond());
+            System.out.printf("%s - %d%n", Thread.currentThread().getName(), LocalDateTime.now().getSecond());
         });
         threadPoolExecutor.execute(testThread);
-        System.out.println(Thread.currentThread().getName());
-        System.out.println(LocalDateTime.now().getSecond());
+        System.out.printf("%s - %d%n", Thread.currentThread().getName(), LocalDateTime.now().getSecond());
     }
 }
