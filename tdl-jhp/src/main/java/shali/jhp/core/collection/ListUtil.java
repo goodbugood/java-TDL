27ca70ecb60312a8fc412b8df8207d300f53d505
shali <shali@gmail.com>
2024-02-25 18:33:43 +0800

    feat: tdl-jhp，实现 list 转 map，和分组 map

--- tdl-jhp/src/main/java/shali/jhp/core/collection/ListUtil.java
+++ tdl-jhp/src/main/java/shali/jhp/core/collection/ListUtil.java
@@ -1,6 +1,8 @@
 package shali.jhp.core.collection;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -38,6 +40,30 @@ public class ListUtil {
             return Stream.concat(listA.stream(), listB.stream()).distinct().collect(Collectors.toList());
         }
         return merge(listA, listB);
+    }
+
+    /**
+     * List<T> 转 Map<String, T>
+     *
+     * @param function 字段
+     * @param list     对象
+     * @param <T>      对象
+     * @param <R>      字段
+     * @return 字段对象的集合
+     */
+    public static <T, R> Map<R, T> toMap(Function<T, R> function, List<T> list) {
+        // 当 key 重复时，key2 覆盖 key1
+        return list.stream().collect(Collectors.toMap(function, Function.identity(), (key1, key2) -> key2));
+    }
+
+    // List<T> 转 Map<String, String>
+    public static <T, R1, R2> Map<R1, R2> toMap(Function<T, R1> function1, Function<T, R2> function2, List<T> list) {
+        return list.stream().collect(Collectors.toMap(function1, function2, (oldValve, newValve) -> newValve));
+    }
+
+    // List<T> 转 Map<String, List<T>>
+    public static <T, R> Map<R, List<T>> groupToMap(Function<T, R> function, List<T> list) {
+        return list.stream().collect(Collectors.groupingBy(function));
     }
 
     private ListUtil() {
