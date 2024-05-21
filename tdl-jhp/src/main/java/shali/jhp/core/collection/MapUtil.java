fd722c7a9d73752a243be2fade8ecf74017e2ea2
Shali <shali@gmail.com>
2024-05-21 23:10:52 +0800

    feat: jhp 添加 map 工具

---
+++ tdl-jhp/src/main/java/shali/jhp/core/collection/MapUtil.java
@@ -0,0 +1,17 @@
+package shali.jhp.core.collection;
+
+/**
+ * 类描述
+ *
+ * @author Shali
+ * @date 02/28/2024
+ */
+public class MapUtil {
+
+    // todo shali 参照 array_keys 获取 map 的所有 key
+
+    // todo shali 参照 array_values 获取 map 的所有 values
+    
+    private MapUtil() {
+    }
+}
